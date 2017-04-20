a26832b3bf6e37312eebaeff67057bab6c7f81cd
unknown <manohar427@gmail.com>
2017-04-20 08:52:08 +0530

    Java loop example

---
+++ CoreJavaLoopsExample/src/com/cf/switchtest/SwitchDemo.java
@@ -0,0 +1,44 @@
+package com.cf.switchtest;
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+		
+		int month = 3;
+		
+		/*switch(month){
+		case 1:
+			System.out.println("JAN");
+			break;
+		case 2:
+			System.out.println("FEB");
+			break;
+		case 3:
+			System.out.println("MAR");
+			break;	
+	     default:
+	    	 System.out.println("UNKNWON");
+				break;
+		}*/
+		
+		/*System.out.println(month == 1);
+		if(month == 1){
+			System.out.println("JAN");
+		}else if(month == 2){
+			System.out.println("FEB");
+		}else if(month == 3){
+			System.out.println("MAR");
+		}else{
+				System.out.println("UNKNOWN");
+		}*/
+
+		/*int x =10;
+		while(x>1){
+			System.out.println("x:"+x);
+			x--;//x = x-1;
+		}
+		*/
+		
+	}
+
+}
